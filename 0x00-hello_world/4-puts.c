aa0dd8e591a0b23b3519c08ddb0fe88f66d46bfd
martin ge <martingeorge191110@gmail.com>
2024-06-20 16:43:48 +0300

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with the puts function
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
