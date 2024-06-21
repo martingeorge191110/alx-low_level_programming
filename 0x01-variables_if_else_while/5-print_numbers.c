074b0a2f4fc9dc362553c7da10ab4b15d8f07d33
martin ge <martingeorge191110@gmail.com>
2024-06-21 15:47:05 +0300

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print digit numbers form 0 to 9
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
