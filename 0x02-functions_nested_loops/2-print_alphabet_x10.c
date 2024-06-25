f682d3fe0232455303469b4c9dab50610988efa1
Martin George <martingeorge191110@gmail.com>
2024-06-25 16:03:08 +0300

    task 2 v2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,9 +6,10 @@
 
 void print_alphabet_x10(void)
 {
-	for (int i = 0 ; i < 10 ; i++)
+	int i,j;
+	for (i = 0 ; i < 10 ; i++)
 	{
-		for (int j = 97 ; j < 123 ; j++)
+		for (j = 97 ; j < 123 ; j++)
 		{
 			_putchar(j);
 		}
