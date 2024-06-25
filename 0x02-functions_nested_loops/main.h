86e4c289ebed7469dd100b068754fde36ded2e3f
Martin George <martingeorge191110@gmail.com>
2024-06-25 22:35:56 +0300

    task 11

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef main_h
 #define main_h
+#include <stdio.h>
 
 int _putchar (char);
 void print_alphabet ();
@@ -12,6 +13,6 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-
+void print_to_98(int n);
 
 #endif
