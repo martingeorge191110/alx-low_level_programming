aefb016a467f779f2de131aef88b2892d3eb0195
Martin George <martingeorge191110@gmail.com>
2024-06-25 22:41:06 +0300

    task 11 v2

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,11 +15,12 @@ void print_to_98(int n)
 		{
 			printf("%d, ", i);
 		}
-	} else
+	}
+	else
 	{
 		for (i = n; i > 98; i--)
 		{
-		   printf("%d, ", i);
+			printf("%d, ", i);
 		}
 	}
 	printf("98\n");
