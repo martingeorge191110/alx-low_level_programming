7fc39da3acde009305485614ff43d929507ecf43
Martin George <martingeorge191110@gmail.com>
2024-06-25 22:23:04 +0300

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: first integer
+ * @b: first integer
+ *
+ * Return: (a+b)
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
