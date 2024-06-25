5101bed79f408c40aa1fa82ef09c6b1d9727c6fd
Martin George <martingeorge191110@gmail.com>
2024-06-25 21:23:31 +0300

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,11 +13,11 @@ void jack_bauer(void)
 	{
 		for (mins = 0 ; mins < 60 ; mins++)
 		{
-			_putchar((hours / 10) + '0');
-			_putchar((hours % 10) + '0');
+			_putchar(hours / 10);
+			_putchar(hours % 10);
 			_putchar(':');
-			_putchar((mins / 10) + '0');
-			_putchar((mins % 10) + '0');
+			_putchar(mins / 10);
+			_putchar(mins % 10);
 			_putchar('\n');
 		}
 	}
