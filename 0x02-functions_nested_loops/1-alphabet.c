de509681941cbe83f41f27524b1b5aa4290a3f1b
Martin George <martingeorge191110@gmail.com>
2024-06-25 15:52:53 +0300

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints all alphabet in lowercase
+*/
+
+void print_alphabet(void)
+{
+	int start;
+	int end;
+
+	start = 97;
+	end = 123;
+
+	while (start < end)
+	{
+	_putchar(start);
+	}
+	_putchar('\n');
+}
