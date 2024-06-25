73ea073833fe44b8394d9df2517e992dd3449307
Martin George <martingeorge191110@gmail.com>
2024-06-25 22:14:18 +0300

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ */
+
+void times_table(void)
+{
+	int counter, index, miniIndex;
+
+	counter = 1;
+	for (index = 0; index < 9 ; index++)
+	{
+		_putchar('0');
+		for (miniIndex = 1; miniIndex < 10; miniIndex++)
+		{
+			int result = miniIndex * counter;
+
+			_putchar(',');
+			_putchar(' ');
+			if (result < 10)
+			{
+				_putchar(' ');
+				_putchar(result + '0');
+			} else
+			{
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+		}
+		_putchar('\n');
+		counter++;
+	}
+}
