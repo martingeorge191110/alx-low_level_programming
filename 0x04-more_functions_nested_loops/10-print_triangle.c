b2280976e4274b38997c16ef2e0509fb0201587c
Martin George <martingeorge191110@gmail.com>
2024-06-27 22:35:25 +0300

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: to determine number of lines and #
+ */
+
+void print_triangle(int size)
+{
+	char space;
+	int i, j, k;
+
+	space = ' ';
+	for (i = 0; i < size ; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			_putchar(space);
+		}
+		for (k = size - i - 1; k < size; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,31 +1,28 @@
 #include <stdio.h>
 
 
-
-
-
-
-void print_square(int size)
+void print_triangle(int size)
 {
-	int i, j;
+    char space;
+    int i, j, k;
 
-	for (i = 0; i < size ; i++)
-	{
-		for (j = 0; j < size; j++)
-		{
-			putchar('#');
-		}
-		putchar('\n');
-	}
-	if (size == 0)
-	{
-		putchar('\n');
-	}
+    space = ' ';
+    for(i = 0; i < size ; i++)
+    {
+        for(j = 0; j < size-i-1; j++)
+        {
+            putchar(space);
+        }
+        for(k = size - i-1 ; k < size; k++)
+        {
+            putchar('#');
+        }
+        putchar('\n');
+    }
 }
-
 
 int main () {
 
-print_square(2);
+print_triangle(10);
     return (0);
 }
