ffd60439a0cb9bf5a745197b917d9d1cf08b1e3f
Martin George <martingeorge191110@gmail.com>
2024-06-27 22:46:48 +0300

    task 10 v2

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,16 +7,13 @@
 
 void print_triangle(int size)
 {
-	if (size <= 0)
-	{
-		_putchar('\n');
-		return;
-	}
 	char space;
 	int i, j, k;
 
 	space = ' ';
-	for (i = 0; i < size ; i++)
+	if (size > 0)
+	{
+		for (i = 0; i < size ; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
@@ -26,6 +23,10 @@ void print_triangle(int size)
 		{
 			_putchar('#');
 		}
+		_putchar('\n');
+	}
+	} else
+	{
 		_putchar('\n');
 	}
 }
--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
 
-
 void print_triangle(int size)
 {
-    char space;
-    int i, j, k;
+	char space;
+	int i, j, k;
 
-    space = ' ';
-    for(i = 0; i < size ; i++)
-    {
-        for(j = 0; j < size-i-1; j++)
-        {
-            putchar(space);
-        }
-        for(k = size - i-1 ; k < size; k++)
-        {
-            putchar('#');
-        }
-        putchar('\n');
-    }
+	space = ' ';
+	if (size > 0)
+	{
+		for (i = 0; i < size ; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			putchar(space);
+		}
+		for (k = size - i - 1; k < size; k++)
+		{
+			putchar('#');
+		}
+		putchar('\n');
+	}
+	} else
+	{
+		putchar('\n');
+	}
 }
+
 
 int main () {
 
-print_triangle(10);
+print_triangle(2);
     return (0);
 }
