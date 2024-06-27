d2ad25d45ffd06a6a47f981d15c96dc232e355bf
Martin George <martingeorge191110@gmail.com>
2024-06-27 16:44:04 +0300

    task 9 v2

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 
-
+/**
+ * main - Entry point
+ *
+ * Description: Fizz-Buzz test
+ *
+ * Return: (0) Success
+ */
 
 int main(void)
 {
-    int i;
+	int i;
 
-    i = 1;
-    printf("%d", i);
-    for (i = 2; i < 101; i++)
-    {
-        if (i % 3 == 0 && i % 5 != 0)
-        {
-            printf(" Fizz");
-        } else if (i % 5 == 0 && i % 3 != 0)
-        {
-            printf(" Buzz");
-        } else if (i % 3 == 0 && i % 5 == 0)
-        {
-            printf(" FizzBuzz");
-        } else
-        {
-            printf(" %d", i);
-        }
-    }
-    putchar('\n');
+	i = 1;
+	printf("%d", i);
+	for (i = 2; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		} else
+		{
+			printf(" %d", i);
+		}
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
