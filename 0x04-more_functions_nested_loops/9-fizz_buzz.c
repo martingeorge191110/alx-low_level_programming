e5f834f8ccb6b94b7e169ff42213722d0b8c405f
Martin George <martingeorge191110@gmail.com>
2024-06-27 16:42:41 +0300

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+
+int main(void)
+{
+    int i;
+
+    i = 1;
+    printf("%d", i);
+    for (i = 2; i < 101; i++)
+    {
+        if (i % 3 == 0 && i % 5 != 0)
+        {
+            printf(" Fizz");
+        } else if (i % 5 == 0 && i % 3 != 0)
+        {
+            printf(" Buzz");
+        } else if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf(" FizzBuzz");
+        } else
+        {
+            printf(" %d", i);
+        }
+    }
+    putchar('\n');
+
+    return (0);
+}
