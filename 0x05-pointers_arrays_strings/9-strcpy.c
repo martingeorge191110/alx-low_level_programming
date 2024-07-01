11430870c0a158cfbc9e569ec8a0e5bc82e7eafa
Martin George <martingeorge191110@gmail.com>
2024-07-01 20:43:03 +0300

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string to another
+ * @dest: string
+ * @src: string
+ *
+ * Return: (dest)
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int start, index;
+
+	start = 0;
+	while (src[start] != '\0')
+	{
+		start++;
+	}
+	start -= 1;
+
+	for (index = 0; index <= start; index++)
+	{
+		dest[index] = src[index];
+		if (index == start)
+		{
+			dest[index] = '\0';
+		}
+	}
+
+	return (dest);
+}
