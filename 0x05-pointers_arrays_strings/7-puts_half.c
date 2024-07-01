750e876571e1e07512329b61bb84104cd84c434c
Martin George <martingeorge191110@gmail.com>
2024-07-01 20:23:40 +0300

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,7 +15,7 @@ void puts_half(char *str)
 		i++;
 	}
 
-	for (j = (i % 2 == 0? i / 2 : (i / 2) + 1); j < i; j++)
+	for (j = (i % 2 == 0 ? i / 2 : (i / 2) + 1); j < i; j++)
 	{
 		_putchar(str[j]);
 	}
