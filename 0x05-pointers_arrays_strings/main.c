03bae448e8afb2351b8bd56fd7608a96fe367623
Martin George <martingeorge191110@gmail.com>
2024-07-01 19:09:14 +0300

    task3

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,22 +1,10 @@
 #include <stdio.h>
 
 
-int _strlen(char *s)
-{
-   int index;
-
-	for (index = 0; s[index] != '\0'; index++)
-	{};
-
-	return (index);
-}
 
 int main()
 {
-   int length = _strlen("martin george");
-
-    printf("%d", length);
-
+    
 
     return (0);
 }
