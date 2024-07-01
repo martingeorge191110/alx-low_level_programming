32ffc54b3231280ac0b1d67f119105fa4014f345
Martin George <martingeorge191110@gmail.com>
2024-07-01 19:16:15 +0300

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - rints a string, in reverse,
+ * @s: string to print
+ */
+
+void print_rev(char *s)
+{
+	int start;
+
+	start = 0;
+	while (s[start] != '\0')
+	{
+		start++;
+	}
+	while (start >= 0)
+	{
+		_putchar(s[start]);
+		start--;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 
+void print_rev(char *s)
+{
+    int start;
 
+    start = 0;
+    while (s[start] != '\0')
+    {
+        start++;
+    }
+    while(start >= 0)
+    {
+        putchar(s[start]);
+        start--;
+    }
+    putchar('\n');
+}
 
 int main()
 {
-    
+    char *strPtr = "martin george";
+    print_rev(strPtr);
 
     return (0);
 }
