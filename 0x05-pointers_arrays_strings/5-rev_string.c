d25b0a27e3eb867779d1cebc0d6344f6e2e431e9
Martin George <martingeorge191110@gmail.com>
2024-07-01 19:49:56 +0300

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,8 +1,13 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - function
+ * @s: string
+ */
 
 void rev_string(char *s)
 {
-   int start, i, counter, length;
+	int start, i, counter, length;
 
 	start = 0;
 	while (s[start] != '\0')
@@ -21,16 +26,4 @@ void rev_string(char *s)
 		counter--;
 	}
 	printf("%s", newStr);
-
-}
-
-int main()
-{
-    char *str = "My School";
-
-    // printf("%s\n", str);
-    rev_string(str);
-    // printf("%s\n", str);
-
-    return (0);
 }
