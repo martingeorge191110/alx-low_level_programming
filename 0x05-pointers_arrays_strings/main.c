d5885f1db5186f99fc2caa9bca25a67130430aad
Martin George <martingeorge191110@gmail.com>
2024-07-01 19:56:43 +0300

    task

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -2,25 +2,28 @@
 
 void rev_string(char *s)
 {
-   int start, i, counter, length;
+   int start, i, counter;
+   char *newStr, temp;
 
 	start = 0;
 	while (s[start] != '\0')
 	{
 		start++;
 	}
-	length = start;
-	char newStr[length];
 
 	start -= 1;
-	counter = start;
+	newStr = s;
+    
 
-	for (i = 0; i <= start; i++)
+	for (i = 0; i < start; i++)
 	{
-		newStr[i] = s[counter];
-		counter--;
+		for (counter = i + 1; counter >= 0; counter--)
+		{
+			temp = *(newStr + counter);
+			*(newStr + counter) = *(newStr + (counter - 1));
+			*(newStr + (counter - 1)) = temp;
+		}
 	}
-	printf("%s", newStr);
 
 }
 
