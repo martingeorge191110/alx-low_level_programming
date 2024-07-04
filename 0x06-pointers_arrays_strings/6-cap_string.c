ce651df21ca3b1659a4dd72c4497c5bdb3c12469
Martin George <martingeorge191110@gmail.com>
2024-07-04 15:30:24 +0300

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,6 +11,7 @@ char *cap_string(char *str)
 {
 	int index;
 
+
 	if (str[0] >= 'a' && str[0] <= 'z')
 	{
 		str[0] -= 32;
@@ -18,9 +19,9 @@ char *cap_string(char *str)
 
 	for (index = 1; str[index] != '\0'; index++)
 	{
-		if ((str[index - 1] < 'a' || str[index - 1] > 'z')
-		&&
-		(str[index - 1] < 'A' || str[index - 1] > 'Z'))
+
+		if ((str[index - 1] < 'a' || str[index - 1] > 'z') &&
+			(str[index - 1] < 'A' || str[index - 1] > 'Z'))
 		{
 			if (str[index] >= 'a' && str[index] <= 'z')
 			{
