8467bf3d96c68e07dcb036ba85aa82ab8e981030
Martin George <martingeorge191110@gmail.com>
2024-07-04 16:24:09 +0300

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,13 +7,14 @@
  * Return: (result) which is integer
  */
 
-int checkCharNumber(char c)
+char checkCharNumber(char c)
 {
-	int i, result;
-	char characters[] = {'a', 'e', 'o', 't', 'l'};
-	char charsInt[] = {4, 3, 0, 7, 1};
+	int i;
+	char result;
+	char *characters = "aeotl";
+	char *charsInt = "43071";
 
-	result = -1;
+	result = -1 + '0';
 	for (i = 0; i < sizeof(characters) / sizeof(char); i++)
 	{
 		if (c == characters[i] || c == characters[i] - 32)
@@ -41,7 +42,7 @@ char *leet(char *str)
 	{
 		checker = checkCharNumber(str[index]);
 
-		if (checker != -1)
+		if (checker != -1 + '0')
 		{
 			str[index] = checker + '0';
 		}
