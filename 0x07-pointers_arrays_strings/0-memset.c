f28069f19d345294dff5ec508a7fc8767f8123de
Martin George <martingeorge191110@gmail.com>
2024-07-07 17:22:00 +0300

    task 0 v3

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,9 +13,11 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++, n--)
+	i = 0;
+	for (; n > 0; n--)
 	{
-		s[i] = b;
+		s[n - n + i] = b;
+		i++;
 	}
 
 	return (s);
--- 0x07-pointers_arrays_strings/test.c
+++ 0x07-pointers_arrays_strings/test.c
@@ -2,15 +2,19 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < n; i++)
-    {
-        s[i] = b;
-    }
+	i = 0;
+	for (; n > 0; n--)
+	{
+		s[n - n + i] = b;
+		i++;
+	}
 
-    return (s);
+
+	return (s);
 }
+
 
 void simple_print_buffer(char *buffer, unsigned int size)
 {
@@ -42,7 +46,7 @@ int main(void)
 {
     char buffer[98] = {0x00};
 
-    // simple_print_buffer(buffer, 98);
+    simple_print_buffer(buffer, 98);
     _memset(buffer, 0x01, 95);
     printf("-------------------------------------------------\n");
     simple_print_buffer(buffer, 98);    
