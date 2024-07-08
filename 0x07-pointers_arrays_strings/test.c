870edc2c165bec5814926106a88dce342a8d0f67
Martin George <martingeorge191110@gmail.com>
2024-07-08 13:19:11 +0300

    task 3

--- 0x07-pointers_arrays_strings/test.c
+++ 0x07-pointers_arrays_strings/test.c
@@ -1,2 +1,39 @@
 #include <stdio.h>
 
+unsigned int _strspn(char *s, char *accept)
+{
+    unsigned int i, j, counter;
+
+
+    counter = 0;
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        int checker = 0;
+        for (j = 0; accept[j] != '\0'; j++)
+        {
+            if (s[i] == accept[j])
+            {
+                counter++;
+                checker = 1;
+                break;
+            } 
+        }
+        if (checker == 0)
+        {
+            break;
+        }
+    }
+
+    return (counter);
+}
+
+int main(void)
+{
+    char *s = "martin, george";
+    char *f = "inramt, ";
+    unsigned int n;
+
+    n = _strspn(s, f);
+    printf("%u\n", n);
+    return (0);
+}
