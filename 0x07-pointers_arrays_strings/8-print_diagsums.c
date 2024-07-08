f63e0174cb522c507d32e6a9ef41757ff0964070
Martin George <martingeorge191110@gmail.com>
2024-07-08 14:53:39 +0300

    task 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,11 +13,11 @@ void print_diagsums(int *a, int size)
 
 	sum1 = 0;
 	sum2 = 0;
-	for (i = 0; i < size;)
+	for (i = 0; i < size; i++)
 	{
 		sum1 = sum1 + a[i];
 		sum2 = sum2 + a[size - i - 1];
-		i = i + 3 + 1;
+		a = a + size;
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
--- 0x07-pointers_arrays_strings/test.c
+++ 0x07-pointers_arrays_strings/test.c
@@ -1,35 +1,36 @@
 #include <stdio.h>
 
-void print_chessboard(char (*a)[8])
+void print_diagsums(int *a, int size)
 {
-    int i, j, length;
-    
-    length = sizeof(a[0]) / sizeof(a[0][0]);
-    for (i = 0; i < length; i++)
-    {
-        for (j = 0; j < length; j++)
-        {
-            putchar(a[i][j]);
-        }
-        putchar('\n');
-    }
+	int i, sum1, sum2;
+
+	sum1 = 0;
+	sum2 = 0;
+	for (i = 0; i < size; i++)
+	{
+        sum1 = sum1 + a[i];
+		sum2 = sum2 + a[size - i - 1];
+        a = a + size;
+	}
+	printf("%d, %d\n", sum1, sum2);
 }
 
 
 int main(void)
 {
-    char board[8][8] = {
-        {'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
-        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-        {'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
+    int c3[3][3] = {
+        0, 1, 5,
+        10, 11, 12,
+        1000, 101, 102
     };
-
-    // printf("%d" , sizeof(board) / sizeof(board[0]));
-    print_chessboard(board);
+    int c5[5][5] = {
+        0, 1, 5, 12124, 1234,
+        10, 11, 12, 123521, 12512,
+        1000, 101, 102, 12545, 214543435,
+        100, 1012451, 11102, 12545, 214543435,
+        10, 12401, 10452, 11542545, 1214543435,
+    };
+    print_diagsums((int *)c3, 3);
+    print_diagsums((int *)c5, 5);
     return (0);
 }
