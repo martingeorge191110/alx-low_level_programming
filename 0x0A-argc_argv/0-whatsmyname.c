f05a982d56537c4d5d69fc159f207f977a473462
Martin George <martingeorge191110@gmail.com>
2024-07-12 15:43:25 +0300

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: argument counter
+ * @argv: argument value
+ *
+ * Return: (0) Succes
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
