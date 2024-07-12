75e0e215bb4cdf235ef01325edfd170768b75ec5
Martin George <martingeorge191110@gmail.com>
2024-07-12 15:52:20 +0300

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives.
+ * @argc: argument counter
+ * @argv: argument value
+ *
+ * Return: (0) Success
+ */
+
+int main(int argc, char *argv[])
+{
+	for (int i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
