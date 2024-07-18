e9d1042cd47b2feca155824def3388d4c1f0e935
Martin George <martingeorge191110@gmail.com>
2024-07-18 16:19:34 +0300

    task 100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: old pointer
+ * @old_size: old size
+ * @new_size: new size
+ *
+ * Return: (ptr) Success
+ * otherwise - (NULL)
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *newPtr;
+
+	newPtr = (void *)malloc(new_size);
+
+	if (newPtr == NULL)
+		return (NULL);
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+		return (newPtr);
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	free(ptr);
+	return (newPtr);
+}
