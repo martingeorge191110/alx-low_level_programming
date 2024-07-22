98df6dd6415baa6c7a1c8548ad0f81701b236f4c
Martin George <martingeorge191110@gmail.com>
2024-07-22 13:55:43 +0300

    task 1 and 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dog - new type struct dog
@@ -8,12 +10,14 @@
  * @owner: dog's owner
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
