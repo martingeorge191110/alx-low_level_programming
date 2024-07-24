fff7d4bebc5ad442ba57b8069616925f7d34c82a
Martin George <martingeorge191110@gmail.com>
2024-07-24 17:24:03 +0300

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,46 @@
+#include "3-calc.h"
+
+/**
+ * main - Entry point
+ * @argv: argument value
+ * @argc: argument counter
+ *
+ * Return: (0) Success
+ */
+
+int main(int argc, char *argv[])
+{
+	int n1, n2;
+	char *operator;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+	operator = argv[2];
+
+	if (get_op_func(operator) == NULL || operator[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if (*(operator) == '/' && n2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	if (*(operator) == '%' && n2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(operator)(n1, n2));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum of two integers
+ * @a: int one
+ * @b: int two
+ *
+ * Return: (a + b) which is the result of summition
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference between two integers
+ * @a: int one
+ * @b: int two
+ *
+ * Return: (a - b) which is the result of difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product of two integers
+ * @a: int one
+ * @b: int two
+ *
+ * Return: (a * b)
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of two integers
+ * @a: int one
+ * @b: int two
+ *
+ * Return: (a / b) which is the result of divison
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder ot the division of two integers
+ * @a: int one
+ * @b: int two
+ *
+ * Return: (a % b) which is the remainder ot the division
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
