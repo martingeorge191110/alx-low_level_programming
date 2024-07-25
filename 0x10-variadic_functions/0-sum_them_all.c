31a043f103f5264aac55b5967dc33d43174bf84e
Martin George <martingeorge191110@gmail.com>
2024-07-25 15:12:26 +0300

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,19 +9,22 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list valist;
+	va_list ptr;
 	unsigned int i;
-	int sum = 0;
+	int sum;
 
 	if (n == 0)
 		return (0);
 
-	va_start(valist, n);
+	va_start(ptr, n);
 
+	sum = 0;
 	for (i = 0; i < n; i++)
-		sum += va_arg(valist, int);
+	{
+		sum += va_arg(ptr, int);
+	}
 
-	va_end(valist);
+	va_end(ptr);
 
 	return (sum);
 }
