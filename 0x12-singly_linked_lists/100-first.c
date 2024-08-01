5018a115cb58aee592c439f419659e0eae0b4898
Martin George <martingeorge191110@gmail.com>
2024-08-01 16:14:47 +0300

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) pre_main(void);
+
+/**
+ * pre_main - Function to be executed before main.
+ * It prints the specified message.
+ */
+
+void pre_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	"I bore my house upon my back!\n");
+}
