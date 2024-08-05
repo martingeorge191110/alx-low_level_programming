5d474166a5f94fb204b1e2c6ab2a6dcf1c590170
Martin George <martingeorge191110@gmail.com>
2024-08-05 20:05:58 +0300

    task 5 v2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,8 +9,9 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (*head != NULL)
-	{
+	if (!head)
+		return;
+
 		temp = *head;
 		while (temp)
 		{
@@ -18,5 +19,6 @@ void free_listint2(listint_t **head)
 			free(*head);
 			*head = temp;
 		}
-	}
+
+	*head = NULL;
 }
