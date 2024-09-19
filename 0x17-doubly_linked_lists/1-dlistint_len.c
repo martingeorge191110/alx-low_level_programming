6611cfc1cef88a168440ebe99bdcb2ef520dbb29
Martin George <martingeorge191110@gmail.com>
2024-09-19 16:57:08 +0300

    Doubly Linked List Project, Task [1]

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Function to returns the number of elements in a dlistint_t list
+ * @h: pointer points to the head of the list
+ *
+ * Return: (counter) which is the number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t counter = 0;
+
+	while (h)
+	{
+		h = h->next;
+		counter++;
+	}
+
+	return (counter);
+}
