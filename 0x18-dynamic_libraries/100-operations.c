3654f525cbb1d7e7f33268daa3999add9a2c6509
Martin George <martingeorge191110@gmail.com>
2024-09-23 15:21:01 +0300

    Fynamic Libraries Project, Task [100]

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+/**
+* add - Function to add two numbers
+* @i: First number
+* @k: Second Numb
+* Return: result of sumition
+*/
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
+
+/**
+* sub - Function to subtracts two numbers
+* @i: First number
+* @k: Second Numb
+* Return: result of Result of Subtraction
+*/
+
+int sub(int n1, int n2)
+{
+	return (n1 - n2);
+}
+
+/**
+* mul - multiplies two numbers
+* @i: First number
+* @k: Second Numb
+* Return: result of multiplication
+*/
+
+int mul(int n1, int n2)
+{
+	return (n1 * n2);
+}
+
+/**
+* div - divideds two numbers
+* @i: First number
+* @k: Second Numb
+* Return: result of dividend numbers
+*/
+
+int div(int n1, int n2)
+{
+	return (n1 / n2);
+}
+
+/**
+  mod - finds the modulus of two numbers
+  @i: First number
+  @k: Second Number
+  Return: Result of modulus
+*/
+
+int mod(int n1, int n2)
+{
+	return (n1 % n2);
+}
