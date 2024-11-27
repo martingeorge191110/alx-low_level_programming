e3e74976096ef3875f304ad718424a18ddd1e750
Martin George <martingeorge191110@gmail.com>
2024-11-27 16:02:01 +0200

    Task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,11 +4,18 @@
 
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid.
+
+    grid is a list of list of integers:
+        0 represents a water zone
+        1 represents a land zone
+        One cell is a square with side length 1
+        Grid cells are connected horizontally/vertically (not diagonally).
+        Grid is rectangular, width and height don’t exceed 100
     """
     w = len(grid[len(grid) - len(grid)])
     h = len(grid)
 
-    parm =  0
+    parm = 0
     for i in range(h):
         for j in range(w):
             if grid[i][j] == 1:
