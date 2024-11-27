8741d904c76e354d3db577a25ef1ba05744e9b1d
Martin George <martingeorge191110@gmail.com>
2024-11-27 15:58:49 +0200

    Task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Defines an island perimeter measuring function."""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid.
+    """
+    w = len(grid[len(grid) - len(grid)])
+    h = len(grid)
+
+    parm =  0
+    for i in range(h):
+        for j in range(w):
+            if grid[i][j] == 1:
+                parm = parm + 4
+                if (j > 0 and grid[i][j - 1] == 1):
+                    parm = parm - 2
+                if (i > 0 and grid[i - 1][j] == 1):
+                    parm = parm - 2
+    return (parm)
